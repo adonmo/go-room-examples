8f0fb627371ccf6a4ad40ef32fb497d2fae5119c
Aditya Patwardhan <gamble09D@gmail.com>
2020-06-28 23:39:18 +0530

    Move transaction out of ORM calls

--- adapter/object_store.go
+++ adapter/object_store.go
@@ -25,11 +25,19 @@ type ObjectStoreEntityModel struct {
 
 //ObjectStoreORMAdapter Adapter for BoltDB to work with Room
 type ObjectStoreORMAdapter struct {
-	db *bolt.DB
+	db        *bolt.DB
+	currentTx *bolt.Tx
 }
 
 //NewORMAdapter Constructor
 func NewORMAdapter(db *bolt.DB) *ObjectStoreORMAdapter {
+	return &ObjectStoreORMAdapter{
+		db: db,
+	}
+}
+
+//NewORMAdapterForTransaction Constructor
+func NewORMAdapterForTransaction(db *bolt.DB, currentTx *bolt.Tx) *ObjectStoreORMAdapter {
 	return &ObjectStoreORMAdapter{
 		db: db,
 	}
@@ -56,85 +64,83 @@ func (oAdap *ObjectStoreORMAdapter) HasTable(entity interface{}) bool {
 
 //CreateTable Create tables for given entities
 func (oAdap *ObjectStoreORMAdapter) CreateTable(entities ...interface{}) orm.Result {
-	err := oAdap.db.Update(func(tx *bolt.Tx) error {
-		for _, entity := range entities {
-			modelDef := oAdap.GetModelDefinition(entity)
-			if isValidModel(modelDef) {
-				bucket, err := tx.CreateBucket([]byte(modelDef.TableName))
-				if err != nil || bucket == nil {
-					return fmt.Errorf("Unable to create bucket for %+v", modelDef)
-				}
-			} else {
-				return fmt.Errorf("Invalid Model Def: %+v", modelDef)
+	var resultErr error
+	for _, entity := range entities {
+		modelDef := oAdap.GetModelDefinition(entity)
+		if isValidModel(modelDef) {
+			bucket, err := oAdap.currentTx.CreateBucket([]byte(modelDef.TableName))
+			if err != nil || bucket == nil {
+				resultErr = fmt.Errorf("Unable to create bucket for %+v", modelDef)
 			}
+		} else {
+			resultErr = fmt.Errorf("Invalid Model Def: %+v", modelDef)
 		}
-		return nil
-	})
+	}
 
 	return orm.Result{
-		Error: err,
+		Error: resultErr,
 	}
 }
 
 //TruncateTable Clear out a table
 func (oAdap *ObjectStoreORMAdapter) TruncateTable(entity interface{}) orm.Result {
-	err := oAdap.db.Update(func(tx *bolt.Tx) error {
-		bucket := oAdap.GetBucketForEntity(entity)
-		if bucket == nil {
-			return fmt.Errorf("No table for entity")
-		}
+	var resultErr error
+	bucket := oAdap.GetBucketForEntity(entity)
+	if bucket == nil {
+		resultErr = fmt.Errorf("No table for entity")
+	}
 
-		keyList := [][]byte{}
-		bucket.ForEach(func(k, v []byte) error {
-			keyList = append(keyList, k)
-			return nil
-		})
-
-		for _, k := range keyList {
-			bucket.Delete(k)
-		}
-
+	keyList := [][]byte{}
+	bucket.ForEach(func(k, v []byte) error {
+		keyList = append(keyList, k)
 		return nil
 	})
 
+	for _, k := range keyList {
+		bucket.Delete(k)
+	}
+
 	return orm.Result{
-		Error: err,
+		Error: resultErr,
 	}
 }
 
 //Create Add the entity to the table
 func (oAdap *ObjectStoreORMAdapter) Create(entity interface{}) orm.Result {
-	err := oAdap.db.Update(func(tx *bolt.Tx) error {
-		bucket := oAdap.GetBucketForEntity(entity)
-		if bucket == nil {
-			return fmt.Errorf("No table for entity")
+	bucket := oAdap.GetBucketForEntity(entity)
+	if bucket == nil {
+		return orm.Result{
+			Error: fmt.Errorf("No table for entity"),
 		}
+	}
 
-		nextSeq, err := bucket.NextSequence()
-		if err != nil {
-			return err
+	nextSeq, err := bucket.NextSequence()
+	if err != nil {
+		return orm.Result{
+			Error: err,
 		}
-		nextID := int(nextSeq)
+	}
+	nextID := int(nextSeq)
 
-		val := reflect.ValueOf(entity).FieldByName("ID")
-		if val.IsValid() && val.CanSet() && val.Kind() == reflect.Int {
-			val.SetInt(int64(nextID))
+	val := reflect.ValueOf(entity).FieldByName("ID")
+	if val.IsValid() && val.CanSet() && val.Kind() == reflect.Int {
+		val.SetInt(int64(nextID))
+	}
+
+	buf, err := json.Marshal(entity)
+	if err != nil {
+		return orm.Result{
+			Error: err,
 		}
+	}
 
-		buf, err := json.Marshal(entity)
-		if err != nil {
-			return err
-		}
-
-		bucket.Put(Itob(nextID), buf)
-		return nil
-	})
-
+	bucket.Put(Itob(nextID), buf)
 	return orm.Result{
-		Error: err,
+		Error: nil,
 	}
 }
 
+//Itob Convert integer to bytes
 func Itob(v int) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, uint64(v))
@@ -143,20 +149,19 @@ func Itob(v int) []byte {
 
 //DropTable Drop a table
 func (oAdap *ObjectStoreORMAdapter) DropTable(entities ...interface{}) orm.Result {
-	err := oAdap.db.Update(func(tx *bolt.Tx) error {
-		for _, entity := range entities {
-			modelDef := oAdap.GetModelDefinition(entity)
-			err := tx.DeleteBucket([]byte(modelDef.TableName))
-			if err != nil {
-				return err
+
+	for _, entity := range entities {
+		modelDef := oAdap.GetModelDefinition(entity)
+		err := oAdap.currentTx.DeleteBucket([]byte(modelDef.TableName))
+		if err != nil {
+			return orm.Result{
+				Error: err,
 			}
 		}
-
-		return nil
-	})
+	}
 
 	return orm.Result{
-		Error: err,
+		Error: nil,
 	}
 }
 
@@ -193,29 +198,26 @@ func (oAdap *ObjectStoreORMAdapter) GetUnderlyingORM() interface{} {
 //GetLatestSchemaIdentityHashAndVersion Get latest metadata for Room
 func (oAdap *ObjectStoreORMAdapter) GetLatestSchemaIdentityHashAndVersion() (identityHash string, version int, err error) {
 	var latestMetadata *room.GoRoomSchemaMaster
-	err = oAdap.db.View(func(tx *bolt.Tx) error {
-		bucket := oAdap.GetBucketForEntity(room.GoRoomSchemaMaster{})
-		if bucket == nil {
-			return fmt.Errorf("No such table")
-		}
+	bucket := oAdap.GetBucketForEntity(room.GoRoomSchemaMaster{})
+	if bucket == nil {
+		err = fmt.Errorf("No such table")
+		return
+	}
 
-		err := bucket.ForEach(func(k, v []byte) error {
-			metadata := room.GoRoomSchemaMaster{}
-			err := json.Unmarshal(v, &metadata)
-			if err != nil {
-				return err
-			}
-
-			latestMetadata = &metadata
-			return nil
-		})
-
+	err = bucket.ForEach(func(k, v []byte) error {
+		metadata := room.GoRoomSchemaMaster{}
+		err := json.Unmarshal(v, &metadata)
 		if err != nil {
 			return err
 		}
 
+		latestMetadata = &metadata
 		return nil
 	})
+
+	if err != nil {
+		return
+	}
 
 	if latestMetadata != nil {
 		identityHash = latestMetadata.IdentityHash
@@ -228,7 +230,7 @@ func (oAdap *ObjectStoreORMAdapter) GetLatestSchemaIdentityHashAndVersion() (ide
 //DoInTransaction Runs the DB operations in a single transaction
 func (oAdap *ObjectStoreORMAdapter) DoInTransaction(fc func(wrappedORM orm.ORM) error) (err error) {
 	boltTransactionFunc := func(tx *bolt.Tx) error {
-		return fc(NewORMAdapter(tx.DB()))
+		return fc(NewORMAdapterForTransaction(tx.DB(), tx))
 	}
 
 	return oAdap.db.Update(boltTransactionFunc)
